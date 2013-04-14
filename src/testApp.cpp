85d5e2aa9d7d55fc3ba9fe51264afd0624b998c8
Weidong Yang <wdyangmail@yahoo.com>
2013-04-14 00:15:02 -0700

    added backdrop, color change of backdrop

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -14,12 +14,22 @@ void testApp::setup()
 	ofEnableAlphaBlending();
 	//ofEnableNormalizedTexCoords();
 	//ofHideCursor();
-	
+
+    backdrop_r = 0.4;
+    backdrop_g = 0.6;
+    backdrop_b = 1.0;
+    backdrop_a = 0.2;
+    
+//    drawGUI=true;
+//    setGUI();
+//    gui->setDrawBack(false);
+    
+    
 	glNewList(1, GL_COMPILE);
 	glutSolidSphere(1, 40, 40);
 	glEndList();
 	
-	boidNum = 400;
+	boidNum = 10;
 	target = ofVec3f(0, 0, 0);
 	
 	for (int i = 0; i < boidNum; i++)
@@ -32,13 +42,22 @@ void testApp::setup()
 	}
 	
 	cam.setDistance(600);
+    cam.disableMouseInput();
+//    cam.setNearClip(50);
+//    cam.setFarClip(1000);
+    fbo.allocate(ofGetWidth(), ofGetHeight());
+    backdrop.loadImage("images/clouds.jpg");
 	
-	GLfloat color[] = { 0.2, 0.2, 1.0 };
+    float backdroplight = 1.0;
+	GLfloat color[] = { backdroplight, backdroplight, backdroplight };
 //	GLfloat color[] = { 1.0, 1.0, 1.0 };
 	
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
+    glEnable(GL_DEPTH_TEST);
+
 	glLightfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, color);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, color);
     
     tuioClient.start(3333);
 }
@@ -100,34 +119,100 @@ float testApp::distance(ofVec3f &x0, ofVec3f &x1){
 //--------------------------------------------------------------
 void testApp::draw()
 {
+//	fbo.begin(false);
+    //    ofClear(0,0,0);
+//    ofClear(0,0,0,1);
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+//    ofEnableAlphaBlending();
+//    ofSetColor(2, 2, 2);
+//    backdrop_a=0.1;
+    GLfloat color[] = { backdrop_r , backdrop_g, backdrop_b, backdrop_a};
+//    GLfloat color[] = { 1, 0.1, 0.1, 0.2};
+    
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, color);
+    backdrop.draw(0,0, ofGetWidth(), ofGetHeight());
+//    ofDisableAlphaBlending();
+    cam.begin();
 	glEnable(GL_DEPTH_TEST);
-	
-	cam.begin();
+//    ofScale(1,-1,1);
+    glDepthFunc(GL_ALWAYS);
+//    glDisable(GL_DEPTH_TEST);
+//    ofDrawGrid();
 	
 	for (int i = 0; i < boidNum; i++)
 	{
 		glPushMatrix();
 		glTranslatef(boids[i].position.x, boids[i].position.y, boids[i].position.z);
 		
-		GLfloat color[] = { 0.2, 0.8, 0.8, 1.0 };
+//		GLfloat color[] = { 0.2, 0.8, 0.8, 1.0 };
+		GLfloat color[] = { 1, 1, 1, 1.0 };
 //		GLfloat color[] = { 0.8, 0.8, 0.8, 1.0 };
 		
 		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, color);
 		glCallList(1);
 		glPopMatrix();
 	}
-	
+//	glDepthFunc(GL_LESS);
 	cam.end();
+//    ofSetColor(255, 255, 255);
+//    ofEnableAlphaBlending();
+//    backdrop.draw(0,0);
+//    ofDisableAlphaBlending();
+
+//    fbo.end();
+//    fbo.draw(0, 0, ofGetWidth(), ofGetHeight());
+}
+
+float increment(float val){
+    float delta = 0.01;
+    val += delta;
+    return (val>1) ? 1 : val;
+}
+
+float decrement(float val){
+    float delta = 0.01;
+    val -= delta;
+    return (val<0) ? 0 : val;
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
+    float delta = 0.01;
     switch(key){
-            case 'f':
+        case 'f':
             ofToggleFullscreen();
             break;
+        case 'h':
+            gui->toggleVisible();
+            break;
+        case 'r':
+            backdrop_r = increment(backdrop_r);
+            break;
+        case 'R':
+            backdrop_r = decrement(backdrop_r);
+            break;
+        case 'g':
+            backdrop_g = increment(backdrop_g);
+            break;
+        case 'G':
+            backdrop_g = decrement(backdrop_g);
+            break;
+        case 'b':
+            backdrop_b = increment(backdrop_b);
+            break;
+        case 'B':
+            backdrop_b = decrement(backdrop_b);
+            break;
+        case 'a':
+            backdrop_a = increment(backdrop_a);
+            break;
+        case 'A':
+            backdrop_a = decrement(backdrop_a);
+            break;
     }
+    cout<<backdrop_r<<" "<<backdrop_g<<" "<<backdrop_b<<" "<<backdrop_a<<endl;
 }
 
 //--------------------------------------------------------------
@@ -138,23 +223,41 @@ void testApp::keyReleased(int key){
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
     float framex, framey;
-    framex =(x-1024.0/2)/1024.0*300;
-    framey =-(y-768.0/2)/768.0*300;
+    float w, h;
+    w=ofGetWidth();
+    h=ofGetHeight();
+    framex =(x-w/2)*10;
+    framey =-(y-h/2)*10;
     cout<<x<<" "<<y<<" --> "<<framex<<" "<<framey<<endl;
     for(int i=0; i<boidNum; i++){
-        if(ofRandom(100)>60)
+        if(ofRandom(100)>30)
             boids[i].seek(ofVec3f(framex, framey, 100));
     }
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
+    float framex, framey;
+    float w, h;
+    w=ofGetWidth();
+    h=ofGetHeight();
+    framex =(x-w/2)*0.7;
+    framey =-(y-h/2)*0.7;
+
+	boidNum++;
+	target = ofVec3f(0, 0, 0);
+	
+    SteeredVehicle v(ofRandom(20)-10+framex, ofRandom(20)-10+framey, ofRandom(100)-50+100);
+    v.maxForce = 0.5f;
+    v.inSightDist = 60.0f;
+    boids.push_back(v);
+    follow.push_back(ofRandom(100)>70); //if true, boid will chase the tuio target
+    cout<<"added a boid"<<endl;
 
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
 }
 
 //--------------------------------------------------------------
@@ -175,4 +278,40 @@ void testApp::gotMessage(ofMessage msg){
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo){ 
 
+}
+void testApp::setGUI(){
+    backdrop_r=1; backdrop_g=1; backdrop_b=1; backdrop_a=1;
+    gui = new ofxUICanvas(0, 0, 255, ofGetHeight());
+    gui->addToggle("drawGUI", drawGUI);
+    gui->addSlider("backdrop_r", 0, 1, backdrop_r);
+    gui->addSlider("backdrop_g", 0, 1, backdrop_g);
+    gui->addSlider("backdrop_b", 0, 1, backdrop_b);
+    gui->addSlider("backdrop_a", 0, 1, backdrop_a);
+    ofAddListener(gui->newGUIEvent, this, &testApp::guiEvent);
+}
+void testApp::guiEvent(ofxUIEventArgs &e){
+    string name=e.widget->getName();
+    cout<<"gui: "<<name<<endl;
+    int kind=e.widget->getKind();
+    if(name=="drawGUI"){
+        ofxUIToggle *toggle = (ofxUIToggle *)e.widget;
+        drawGUI = toggle->getValue();
+        cout<<"drawGUI is "<<drawGUI<<endl;
+    }if(name=="backdrop_r"){
+        ofxUISlider *slider = (ofxUISlider *)e.widget;
+        backdrop_r = slider->getValue();
+        cout<<"backdrop_r: "<<backdrop_r<<endl;
+    }if(name=="backdrop_g"){
+        ofxUISlider *slider = (ofxUISlider *)e.widget;
+        backdrop_g = slider->getValue();
+        cout<<"backdrop_g: "<<backdrop_g<<endl;
+    }if(name=="backdrop_b"){
+        ofxUISlider *slider = (ofxUISlider *)e.widget;
+        backdrop_b = slider->getValue();
+        cout<<"backdrop_b: "<<backdrop_b<<endl;
+    }if(name=="backdrop_a"){
+        ofxUISlider *slider = (ofxUISlider *)e.widget;
+        backdrop_a = slider->getValue();
+        cout<<"backdrop_a: "<<backdrop_a<<endl;
+    }
 }
--- src/testApp.h
+++ src/testApp.h
@@ -3,26 +3,34 @@
 #include "ofMain.h"
 #include "ofxBoids.h"
 #include "ofxTuio.h"
+#include "ofxUI.h"
 
 class testApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+    void setup();
+    void update();
+    void draw();
 
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
 	
+    void setGUI();
+    void guiEvent(ofxUIEventArgs &e);
+    ofxUICanvas *gui;
+    bool drawGUI;
+    float backdrop_r, backdrop_g, backdrop_b, backdrop_a;
 	
 	ofEasyCam cam;
+    ofFbo fbo;
+    ofImage backdrop;
 	
 	int boidNum;
 	ofVec3f target;
